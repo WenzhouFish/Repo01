185f9a5dc180185a09ea315cb82a9db5f975d00d
WenzhouFish <1273722173@qq.com>
2023-11-03 11:05:18 +0800

    commit 11 3 hanoi

--- Hanoi_11_3.cpp
+++ Hanoi_11_3.cpp
@@ -1,20 +1,22 @@
-#include<iostream>
-using namespace std ;
-void hannoi(int n,char A,char B,char C) {
-	if ( n == 1 ) {
-		cout << n << ":" << A << "->" << C << endl;  
-	}
-	else {
-		hannoi ( n-1 , A , C , B ) ;  
-		cout << n << ":" << A << "->" << C << endl; 
-		hannoi ( n-1 , B , A , C ) ; 
-	}
-} 
-int main () {
-    int n ;
-    cin >> n ;
-    cout << endl ;
-    hannoi ( n , 'A' , 'B' , 'C' ) ;
+#include <iostream>
+using namespace std;
+
+int n;
+char A, B, C;
+
+void hanoi(int n, char a, char b, char c) {
+    if (n == 1) {
+        cout << n << ":" << a << "->" << c << endl;
+    } else {
+        hanoi(n - 1, a, c, b);
+        cout << n << ":" << a << "->" << c << endl;
+        hanoi(n - 1, b, a, c);
+    }
+}
+
+int main() {
+    cin >> n >> A >> B >> C;
+    hanoi(n, A, B, C);
 	system ( "pause" ) ;
-    return 0 ;
+    return 0;
 }
